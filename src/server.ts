e0cf6c6ce2a405d9cddfad992dd297471fc3f7e0
Albert Dias <albert.uft@gmail.com>
2023-07-19 21:43:01 -0300

    alter port

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log("Api is only port: 3333"))
+app.listen(3335, () => console.log("Api is only port: 3333"));
